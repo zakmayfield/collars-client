5c83378f601254ae0cfdff05cbe65686bec2183a
Zak Mayfield <zakmayfield@gmail.com>
2023-02-16 14:10:59 -0500

    basic styling

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -2,8 +2,8 @@ import { Box, Heading } from '@chakra-ui/react';
 
 export const Landing = () => {
     return (
-        <Box as='section'>
-            <Heading as='h1'>🐶 🐱 Collars Landing Page</Heading>
+        <Box as='section' bg='blue.400'>
+            <Heading as='h1' textAlign='center'>🐶 🐱 Collars Landing Page</Heading>
         </Box>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,6 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <h1>Route: /</h1>
         <Landing />
       </main>
     </>
